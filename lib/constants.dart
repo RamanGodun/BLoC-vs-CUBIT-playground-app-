7147633b9b725d141c0dc4ac017eb652d8bb9004
Roman Godun <4l.roman.godun@gmail.com>
2025-02-13 14:41:55 +0200

    feat: add some classes

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static final appTheme = ThemeData(
+    colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    useMaterial3: true,
+  );
+}
